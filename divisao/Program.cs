d3b3e5135cd08ed2578ab02e1775550fba905c1e
alessandra <alessandra2309@gmail.com>
2023-06-05 17:08:01 -0400

    try cath

--- divisao/Program.cs
+++ divisao/Program.cs
@@ -1,12 +1,27 @@
-﻿public class progam
+﻿
+using System;
+
+
+try
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("informe o primeiro valor: ");
-        int a = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("informe o primeiro valor: ");
-        int b = Convert.ToInt32(Console.ReadLine());
-        int result = a / b;
-        Console.WriteLine(result);
-    }
+    Console.WriteLine("informe o primeiro valor: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("informe o primeiro valor: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    int result = a / b;
+   Console.WriteLine($"O resultado é{result}");
+}catch(DivideByZeroException e)
+{
+   Console.WriteLine($"não é permitido divisão por zero! {e.Message}");
+}catch(InvalidCastException e)
+{
+
+}catch(Exception e)
+{
+
 }
+Console.ReadKey();
+       
+    
